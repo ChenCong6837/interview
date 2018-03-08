4b368c6ca7b760bf3affff24ab21263bdcb01f8e
chencong <337812462@qq.com>
2018-03-08 18:57:27 +0800

    Initial commit

---
+++ this相关.js
@@ -0,0 +1,25 @@
+var names = "A";
+var obj = {
+    names:"B",
+    showName:function(){
+        console.log(this.names);
+    },
+    returnName:function(){
+        return this.names;
+    },
+    returnFunctionName:function(){
+        return function(){
+            console.log(this.names);
+        }
+    }
+    
+}
+obj.showName();                                     //输出什么？   "B"
+obj.returnName();                                   //输出什么？   "B"
+obj.returnFunctionName()();                         //输出什么？   "A"
+obj.showName.call(names);                           //输出什么？   undefined
+obj.returnName.call(names);                         //输出什么？   undefined
+obj.returnFunctionName().call(names)                //输出什么？   undefined
+var newObj = obj.returnFunctionName().bind(window);
+newObj.call(obj)                                    //输出什么？   "A"
+//为什么最后一个输出"A"？因为bind指向this对象后  再一次调用的话  this指向不会被改变
