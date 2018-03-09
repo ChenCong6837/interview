0a26ed67878e95b851037cc499a60c8a473b6b08
chencong <337812462@qq.com>
2018-03-09 21:16:22 +0800

    又是一道this相关的题

--- this相关.js
+++ this相关.js
@@ -36,3 +36,23 @@ var obj = {
 obj.showBig.call(big);          //ƒ big() { [native code] }
 
 //======================================================================================
+function a(a,b,c){
+    console.log(this.length);                 //4
+    console.log(this.callee.length);          //1
+}
+
+function fn(d){
+    arguments[0](10,20,30,40,50);
+}
+
+fn(a,10,20,30);
+
+
+//第一个输出结果:因为this当前指向的是arguments 。 arguments是一个伪数组具备length属性。arguments又是保存函数的实参。
+//fn调用的时候传入4个实参。所以arguments长度为4。这个时候arguments[0] 等同于 arguments.a调用这个函数。
+//所以this指向的是arguments这个伪数组也是(对象)
+
+//第二个输出结果：callee是arguments的一个属性,主要返回当前arguments直属的函数体。所以this.callees是返回fn 。
+//每一个函数有一个length属性主要用来返回函数的形参的所以就是1。
+
+//======================================================================================
