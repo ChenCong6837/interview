d47279f8572138e9f74280672ff0126491713348
chencong <337812462@qq.com>
2018-03-28 13:40:14 +0800

    2.关于变量定义的一道题

--- 25个最基本的JavaScript面试问题及答案.js
+++ 25个最基本的JavaScript面试问题及答案.js
@@ -1,12 +1,13 @@
 /*
  * @Author: ChenCong 
  * @Date: 2018-03-27 19:36:08 
- * @Last Modified by:   ChenCong 
- * @Last Modified time: 2018-03-27 19:36:08 
+ * @Last Modified by: ChenCong
+ * @Last Modified time: 2018-03-28 13:39:29
  */
 
 //转载自：http://www.codeceo.com/25-essential-javascript-interview-questions.html
 
+//======================================================================================
 //1. 使用 typeof bar === "object" 来确定 bar 是否是对象的潜在陷阱是什么？如何避免这个陷阱？
 
 /**
@@ -39,3 +40,39 @@ console.log((bar !== null) && (typeof bar === "object") && (toString.call(bar) !
 console.log((bar !== null) && (typeof bar === "object") && (! $.isArray(bar)));
 
 //======================================================================================
+//2. 下面的代码将输出什么到控制台，为什么？
+
+(function() {
+    var a = b = 3;
+})();
+
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
+
+/**
+ * 由于 a 和 b 都定义在函数的封闭范围内，并且都始于 var 关键字，大多数 JavaScript 开发人员期望 typeof a 和 typeof b 在
+ * 上面的例子中都是 undefined。
+ * 然而，事实并非如此。这里的问题是，大多数开发人员将语句 var a = b = 3; 错误的认为是以下声明的简写：
+ */
+
+ var b = 3;
+ var a = 3;
+ 
+/**
+ * 但事实上， var a = b = 3; 实际是以下声明的简写：
+ */
+
+  b = 3;
+  var a = b;
+
+/**
+ * 因此（如果你不使用严格模式的话），该代码段的输出是：
+ * 
+ * a defined? false
+ * b defined? true
+ * 
+ * 但是，b 如何才能被定义在封闭函数的范围之外呢？是的，既然语句 var a = b = 3; 是语句 b = 3; 和 var a = b; 的简写，
+ * b 最终成为了一个全局变量（因为它没有前缀 var 关键字），因此仍然在范围内甚至函数之外。
+ * 需要注意的是，在严格模式下（即使用 use strict），语句 var a = b = 3; 将生成 ReferenceError: b is not defined 的
+ * 运行时错误，从而避免任何否则可能会导致的 headfakes /bug。（还是你为什么理所当然地在代码中使用 use strict 的最好例子！）
+ */
